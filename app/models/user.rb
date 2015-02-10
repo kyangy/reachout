878a25977b98295f16de8fa5e480e0065087143c
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-02-09 17:50:38 -0800

    included has_secure_password and created static_pages controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+	has_secure_password
+
 	validates :username, presence: true
 	validates :name, presence: true
 	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: {case_sensitive: false}
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :users
+  get 'static_pages/home'
+
+  resources :users, only: [:create]
+  get '/signup' => 'users#new', as: :signup
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
