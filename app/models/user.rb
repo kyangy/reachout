9ffbeef5c5962ed59e8a838aadcc0cd4c973cb3a
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-02-09 17:03:47 -0800

    passed some user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+
+	validates :username, presence: true
+	validates :name, presence: true
+	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: {case_sensitive: false}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,29 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   
-	it 'has a factory for user'
-	it 'is invalid without a username'
-	it 'is invalid without a name'
-	it 'is invalid without an email'
+	it 'has a factory for user' do
+		expect(FactoryGirl.build(:user)).to be_valid
+	end
+
+	it 'is invalid without a username' do
+		user = FactoryGirl.build(:user, username: nil)
+		expect(user).to be_invalid
+	end
+
+	it 'is invalid without a name' do
+		user = FactoryGirl.build(:user, name: nil)
+		expect(user).to be_invalid
+	end
+
+	it 'is invalid without an email' do
+		user = FactoryGirl.build(:user, email: nil)
+		expect(user).to be_invalid
+	end
+
+	it 'is invalid if the email is not formatted properly' do
+		user = FactoryGirl.build(:user, email: 'cnjeanacoce')
+		expect(user).to be_invalid
+	end
+
 	it 'should have a password minimum length'
 end
