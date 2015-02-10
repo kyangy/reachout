f0fe39270a0e9c9b981f1350f1dd6ef6cc7440f2
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-02-09 18:54:47 -0800

    working log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by(email: params[:login][:email])
-		if user && user.authenticate(params[:login][:password])
-			session[:user_id] = user.id.to_s
+		user = User.find_by(username: params[:session][:username])
+		if user && user.authenticate(params[:session][:password])
+			log_in user
 			redirect_to root_path
 		else
 			redirect_to login_path
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-		session.delete(:user_id)
+		log_out if logged_in?
 		redirect_to root_path
 	end
 end
