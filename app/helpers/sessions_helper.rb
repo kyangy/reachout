2a18f95442a0a4a9d47989a6425c78161f3ecc20
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-02-09 18:30:39 -0800

    created sessions methods and helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+
+	def logged_in?
+		session[:user_id] != nil
+	end
+
+	def current_user
+		if session[:user_id]
+			@current_user ||= User.find(session[:user_id])
+		end
+	end
+
 end
