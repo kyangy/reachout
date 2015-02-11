706a52d7fc9a6ab789327fd27149af38409304c3
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-02-10 16:35:44 -0800

    added image column to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210031132) do
+ActiveRecord::Schema.define(version: 20150211003351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150210031132) do
     t.string   "duration"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
