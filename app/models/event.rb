b6beeee7c7b11ab240a52f33f0eec6da99e10726
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-02-10 19:15:46 -0800

    made relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ActiveRecord::Base
 
+	has_many :users, through: :user_events
+	has_many :user_events
+
 	mount_uploader :image, AvatarUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
-	has_many :events, through: :v
+	has_many :events, through: :user_events
+	has_many :user_events
 	has_secure_password
 
 	validates :username, presence: true
