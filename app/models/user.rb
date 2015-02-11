b08ef3cc7e44a9edcf1e9ca1d14350d19efe9007
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-02-10 16:30:34 -0800

    generated and mounted Avatar uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
+	mount_uploader :image, AvatarUploader
+
 	validates :username, presence: true
 	validates :name, presence: true
 	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: {case_sensitive: false}, length: {maximum: 255}
